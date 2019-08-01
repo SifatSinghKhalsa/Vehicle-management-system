56f379209858dafd9152bbb5931e5b43734f8e99
Prateek Vachher <vachh007@umn.edu>
2019-07-31 18:13:21 -0700

    Restructured API Design and Added add features

--- app.py
+++ app.py
@@ -6,29 +6,50 @@ app = Flask(__name__)
 def index():
     return "Vehicle Management API is active"
 
-@app.route('/vehicle/<vehicle_number>', methods=['GET'])
+# Information Routing for Querying Information for Database
+
+@app.route('/info/vehicle/<vehicle_number>', methods=['GET'])
 def vehicle_info(vehicle_number):
     return "You're Trying to Access Vehicle Information for Vehicle Number {}".format(vehicle_number)
 
-@app.route('/maintenance/<vehicle_number>', methods=['GET'])
+@app.route('/info/maintenance/<vehicle_number>', methods=['GET'])
 def maintenance_info(vehicle_number):
     return "You're Trying to Access Maintence Records for Vehicle Number {}".format(vehicle_number)
 
-@app.route('/fuel/<vehicle_number>', methods=['GET'])
+@app.route('/info/fuel/<vehicle_number>', methods=['GET'])
 def fuel_info(vehicle_number):
     return "You're Trying to Access Fuel Records for Vehicle Number {}".format(vehicle_number)
 
-@app.route('/dealer/<dealer_id>', methods=['GET'])
+@app.route('/info/dealer/<dealer_id>', methods=['GET'])
 def dealer_info(dealer_id):
     return "You're Trying to Access Dealer Information Records for Dealer ID {}".format(dealer_id)
 
-@app.route('/insurance/<insurance_id>', methods=['GET'])
+@app.route('/info/insurance/<insurance_id>', methods=['GET'])
 def insurance_info(insurance_id):
     return "You're Trying to Access Insurance Information Records for Insurance ID {}".format(insurance_id)
 
-@app.route('/user/<user_id>', methods=['GET'])
+@app.route('/info/user/<user_id>', methods=['GET'])
 def insurance_info(user_id):
     return "You're Trying to Access User Information Records for User ID {}".format(user_id)
+
+# Adding Information Fields to Database
+
+@app.route('/add/vehicle/<vehicle_number>', methods=['GET'])
+def vehicle_add(vehicle_number):
+    return "You're Trying to Access Maintence Records for Vehicle Number {}".format(vehicle_number)
+
+@app.route('/add/dealer/<dealer_id>', methods=['GET'])
+def dealer_add(dealer_id):
+    return "You're Trying to Access Maintence Records for Vehicle Number {}".format(dealer_id)
+
+@app.route('/add/insurance/<insurance_id>', methods=['GET'])
+def insurance_add(insurance_id):
+    return "You're Trying to Access Maintence Records for Vehicle Number {}".format(insurance_id)
+
+@app.route('/add/user/<user_id>', methods=['GET'])
+def user_add(user_id):
+    return "You're Trying to Access Maintence Records for Vehicle Number {}".format(user_id)
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0',port='5000')
